c55ef24c740aa24b31ea86072b397c1ed91391da
Atticus Bross <atb5098@utulsa.edu>
2024-12-03 11:16:13 -0600

    added deep_unpack function and tests

--- common_functions.py
+++ common_functions.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+from types import UnionType
 def rows(values: list, rows2: int) -> list[list]:
     """Breaks data up into a given number of rows
 
@@ -8,3 +10,19 @@ def rows(values: list, rows2: int) -> list[list]:
     for i in range(rows2):
         return_rows.append(values[i * row_len:(i + 1) * row_len])
     return return_rows
+def deep_unpack(seq: Iterable[Iterable], ignores: type | UnionType = str) -> list:
+    """Unpacks an Iterable of Iterables into a single Iterable
+
+    seq: the Iterable
+    ignores: the types of Iterables to ignore"""
+    unpacked: list = []
+    for element in seq:
+        if isinstance(element, Iterable) and not isinstance(element, ignores):
+            # this is to avoid the infinite recursion that occurs because a string contains a string which contains a string, etc.
+            if isinstance(element, str) and len(element) == 1:
+                unpacked.append(element)
+            else:
+                unpacked.extend(deep_unpack(element, ignores))
+        else:
+            unpacked.append(element)
+    return unpacked
--- test_common_functions.py
+++ test_common_functions.py
@@ -6,4 +6,11 @@ def test_rows() -> None:
     assert rows([1, 2, 3, 4], 2) == [[1, 2], [3, 4]]
     assert rows([1, 2, 3, 4, 5, 6], 3) == [[1, 2], [3, 4], [5, 6]]
     assert rows([1, 2, 3, 4, 5, 6], 2) == [[1, 2, 3], [4, 5, 6]]
+def test_deep_unpack() -> None:
+    """Tests the deep_unpack function"""
+    assert deep_unpack([[None, 'abc', 1.2], [True, 3], ['ghf']]) == [None, 'abc', 1.2, True, 3, 'ghf']
+    assert deep_unpack([[['abc', None], [], 2.34], [[], [True, 3]]]) == ['abc', None, 2.34, True, 3]
+    assert deep_unpack([[(1, 2, 3), ('abc', 'efg')], ['abc']], tuple) == [(1, 2, 3), ('abc', 'efg'), 'a', 'b', 'c']
+    assert deep_unpack([[(1, 2, 3), ('abc', 'efg')], ['abc']], tuple | str) == [(1, 2, 3), ('abc', 'efg'), 'abc']
 test_rows()
+test_deep_unpack()
