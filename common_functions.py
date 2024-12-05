e2ba18bae5c93d16b364c2f43bb86a7a4b16d5ba
Atticus Bross <atb5098@utulsa.edu>
2024-12-05 16:27:57 -0600

    added function points and lines functions

--- common_functions.py
+++ common_functions.py
@@ -1,3 +1,4 @@
+from math import sqrt, atan, pi
 from types import UnionType
 from typing import Iterable
 
@@ -63,3 +64,45 @@ def table(col: int, *items: str) -> str:
         header_indication = header_indication + '---|'
     rows2.insert(1, header_indication)
     return '\n'.join(rows2)
+
+
+Number = int | float
+
+
+def function_points(f, start: Number, end: Number, n: int = 101) -> tuple[tuple[Number, Number], ...]:
+    """function_points(f, start, end, n=101) Returns a series of ordered pairs from a given function
+
+    f: a python function that takes one numerical input and produces one numerical output
+    start: the input to start the computation at
+    end: the input to end the computation at
+    n: the number of points to compute"""
+    step: Number = (end - start) / (n - 1)
+    points: tuple = ()
+    for i in range(n):
+        x: Number = i * step
+        y: Number = f(x)
+        points = points + ((x, y),)
+    return points
+
+
+def function_lines(f, start: Number, end: Number, n: int = 100) -> tuple[tuple[Number, Number], ...]:
+    """function_lines(f, start, end, n=100) Returns a series of lines (length, heading) approximating a function
+
+    f: a python function that takes one numerical input and produces one numerical output
+    start: the input to start the computation at
+    end: the input to end the computation at
+    n: the number of lines to compute"""
+    points: tuple = function_points(f, start, end, n + 1)
+    lines: tuple = ()
+    # iterates through every element in points except the last
+    for index, point in enumerate(points[0:-1]):
+        x1: Number = point[0]
+        y1: Number = point[1]
+        x2: Number = points[index + 1][0]
+        y2: Number = points[index + 1][1]
+        length = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        slope: Number = (y2 - y1) / (x2 - x1)
+        radian_heading = atan(slope)
+        heading = radian_heading * 180 / pi
+        lines = lines + ((length, heading),)
+    return lines
--- test_common_functions.py
+++ test_common_functions.py
@@ -54,7 +54,34 @@ def test_table() -> None:
     with StringIO() as test:
         test.write(table(2, 'a', 'b', 'c', 'd', 'e', 'f'))
         assert test.getvalue() == '|a|b|\n|---|---|\n|c|d|\n|e|f|'
+
+
+def test_function_points() -> None:
+    """test_function_points() Tests the function_points function"""
+    assert len(function_points(lambda x: x, 0, 10)) == 101
+    assert function_points(lambda x: x, 0, 100)[50][1] == 50
+    for points in function_points(lambda x: x, 0, 100, 10):
+        assert points[0] == points[1]
+    assert len(function_points(lambda x: x, 0, 100, 10)) == 10
+    for points in function_points(lambda x: x ** 2, 55, 234, 17):
+        assert points[0] ** 2 == points[1]
+    assert function_points(lambda x: x ** 2, 1, 10, 10)[5][1] == 25
+
+
+def test_function_lines() -> None:
+    """test_function_lines() Tests the function_lines function"""
+    assert len(function_lines(lambda x: x, 0, 10)) == 100
+    assert function_lines(lambda x: x, 0, 10, 10)[0][0] == sqrt(2)
+    assert function_lines(lambda x: x, 0, 10, 15)[0][1] == 45
+    for line in function_lines(lambda x: x, 0, 10, 10):
+        assert line[0] == sqrt(2)
+        assert line[1] == 45
+    assert len(function_lines(lambda x: x, 15, 67, 45)) == 45
+    assert function_lines(lambda x: x ** 2, 0, 10, 10)[0][0] == sqrt(2)
+    assert function_lines(lambda x: x ** 2, 0, 10, 10)[0][1] == 45
 test_rows()
 test_deep_unpack()
 test_table_row()
 test_table()
+test_function_points()
+test_function_lines()
